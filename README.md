f5b26fca080909354c8d9f95c51d7a206d9be588
juliandavidmr <anlijudavid@hotmail.com>
2017-03-12 02:06:54 -0500

    improvements

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # uDebug
-Get results of exercises from Udebug
+
+[**English** (In progress)](./docs/en.md)
+
+uDebug es un módulo instalable multiplataforma, disponible para Linux, Windows y Mac OS.
+Permite obtener, visualizar y probar rápidamente ejercicios desarrollados en C++ y Java. *uDebug* corre bajo el motor V8 de Google, por lo tanto es necesario instalar [NodeJS](https://nodejs.org/es) para su ejecución.
+
+## Instalación
+
+### Aún está en proceso de desarrollo, por lo tanto no se encuentra publicado en npm.
+
+```bash
+$ [sudo] npm install -g udebug
+...
+
+$ udebug -h
+# Muestra la ayuda
+```
+
+## Consultar información
+Udebug permite consultar la información basica de un enunciado. Los datos provienen directamente de UVA Judge y uHunt.
+
+```bash
+$ udebug desc 100
+# Donde 100 es el numero del ejercicio según UVA Judge
+```
+
+## Compilar código
+
+### Cpp
+```bash
+$ udebug c hola.cpp
+# o
+$ udebug compile hola.cpp
+```
+
+### Java
+```bash
+$ udebug c hola.java
+# o
+$ udebug compile hola.java
+```
+
+### Especificar directorio
+```bash
+$ udebug c carpeta/hola.cpp
+$ udebug c carpeta/hola.java
+```
--- cli.js
+++ cli.js
@@ -2,26 +2,48 @@
 
 var udebug = require("./lib/udebug.lib");
 var uhunt = require("uhunt-node");
+var compile = require('./lib/compile')
 const prog = require('caporal');
 
 prog
-  .version('1.0.0')
-  // run command
-  .command('run', 'Run file java or cpp')
-  .alias('r')
+  .version(require("./package.json").version)
+  /**
+   * Compile command
+   */
+  .command('compile', 'compile file java or cpp')
+  .alias('c')
   .argument('<file>', 'Codigo fuente .cpp, .java')
   .action((args, options, logger) => {
-    console.log("Ejecuntando codigo: ", args)
-    // logger.log("I'm walking !")
+    console.log("Compilando: ", args.file);
+    var file = args.file;
+    if (file.endsWith('.cpp')) {
+      compile.cpp(file).then(_out => {
+        if(_out) {
+          console.log("Error:", _out);
+        } else {
+          console.log("Compilado");
+        }
+      }, err => {
+        console.log("Error:", err);
+      });
+    } else if (file.endsWith('.java')) {
+
+    } else {
+
+    }
   })
-  // test command
+  /**
+   * test command
+   */
   .command('test', 'Make the player walk')
   .alias('t')
   .argument('<file>', 'Codigo fuente .cpp, .java')
   .action((args, options, logger) => {
     console.log("Probando codigo: ", args)
-    // logger.log("I'm walking !")
   })
+  /**
+   * Describe un ejercicio
+   */
   .command('desc', 'Describe un ejercicio')
   .alias('d')
   .argument('<number>', 'Numero del ejercicio')
@@ -32,6 +54,9 @@ prog
     })
     // logger.log("I'm walking !")
   })
+  /**
+   * Casos de pruebas
+   */
   .command('cases', 'Casos de prueba')
   .alias('c')
   .argument('<number>', 'Numero del ejercicio')
