dbc76c549c558fdd2c9561114863dc1aa42086fd
juliandavidmr <anlijudavid@hotmail.com>
2017-03-12 18:29:41 -0500

    Visual improvements

--- README.md
+++ README.md
@@ -21,20 +21,57 @@ $ udebug -h
 Udebug permite consultar la información basica de un enunciado. Los datos provienen directamente de UVA Judge y uHunt.
 
 ```bash
-$ udebug desc 100
-$ udebug d 100    #=> Usando atajo
-# Donde 100 es el numero del ejercicio según UVA Judge
+$ udebug desc 100    #Nota: udebug d 100 -> Usando atajo
+┌─────┬────────┬────────────────────┬───────┬────────┐
+│ pid │ number │ title              │ limit │ status │
+├─────┼────────┼────────────────────┼───────┼────────┤
+│ 36  │ 100    │ The 3n + 1 problem │ 3000  │ 1      │
+└─────┴────────┴────────────────────┴───────┴────────┘
 ```
 
 ## Consultar mis envíos
 Consulte el listado de envíos realizados por un usuario determinado.
 
 ```bash
+# 706667 es el ID del usuario registrado en UVA Judge.
 $ udebug submissions 706667
-$ udebug s 706667    #=> Usando atajo
-# Donde 706667 es el ID del usuario registrado en UVA Judge.
+┌──────────────┬─────────────────────────┐
+│ name         │ user name               │
+├──────────────┼─────────────────────────┤
+│ Julian David │ anlijudavid@hotmail.com │
+└──────────────┴─────────────────────────┘
+┌───────────┬─────────┬───────────┬──────┬───────┬──────────────────────────┐
+│ Reference │ problem │ result    │ time │ code  │ date                     │
+├───────────┼─────────┼───────────┼──────┼───────┼──────────────────────────┤
+│ 17787180  │ 2253    │ Accepted  │ 0    │ c++11 │ Thu Aug 04 2016 13:34:37 │
+├───────────┼─────────┼───────────┼──────┼───────┼──────────────────────────┤
+│ 18144493  │ 3666    │ Accepted  │ 170  │ java  │ Sat Oct 08 2016 11:18:18 │
+└───────────┴─────────┴───────────┴──────┴───────┴──────────────────────────┘
+...
 ```
 
+## Casos de prueba
+Consulte los casos de prueba disponibles para un ejercicio determinado:
+
+```
+$ udebug cs 100
+┌───┬────────┬────────┬───────┬───────────┬───────┐
+│ # │ Number │ DataID │ User  │ Date      │ Votes │
+├───┼────────┼────────┼───────┼───────────┼───────┤
+│ 5 │ 3      │ 821829 │ Ryuuk │ Fri Jan … │ 3     │
+└───┴────────┴────────┴───────┴───────────┴───────┘
+Case  5  by  Ryuuk :
+2 1596        ┐
+99 9999       │
+1 1999        │
+340 30100     │
+5 6           │= Estos son los casos
+7 10          │
+1 98746       │
+9999 9998     ┘
+                  
+...
+```
 
 ## Compilar código
 
@@ -54,4 +91,12 @@ $ udebug compile hola.java
 ```bash
 $ udebug c carpeta/hola.cpp
 $ udebug c carpeta/hola.java
+```
+
+### Comando de ayuda
+```bash
+$ udebug help <comando>
+
+# Ejemplo:
+$ udebug help submissions
 ```
--- cli.js
+++ cli.js
@@ -2,92 +2,124 @@
 
 var udebug = require("./lib/udebug.lib");
 var uhunt = require("uhunt-node");
-var compile = require('./lib/compile')
+var compile = require('./lib/compile');
+var table = require("./lib/table")
 const prog = require('caporal');
+var spinner = require('./lib/spinner');
 
 prog
   .version(require("./package.json").version)
   /**
    * Compile command
    */
-  .command('compile', 'compile file java or cpp')
+  .command('compile', 'Compile file java or cpp')
   .alias('c')
   .argument('<file>', 'Codigo fuente .cpp, .java')
   .action((args, options, logger) => {
-    console.log("Compilando: ", args.file);
+    spinner.start("Compiling source code " + args.file);
     var file = args.file;
     if (file.endsWith('.cpp')) { // Compile cpp
-      compile.cpp(file).then(_out => {
-        if (_out) {
-          console.log("Error:", _out);
-        } else {
-          console.log("Compilado");
-        }
-      }, err => {
-        console.log("Error:", err);
-      });
-    } else if (file.endsWith('.java')) { // compile java
-      compile.java(file).then(_out => {
-        if (_out) {
-          console.log("Error:", _out);
-        } else {
-          console.log("Compilado");
-        }
-      }, err => {
-        console.log("Error:", err);
-      });
-    } else {
 
+      compile.cpp(file).then(_out => {
+          spinner.stop();
+          console.log(_out ? _out : 'Compilado')
+        },
+        err => console.log("Error:", err));
+
+    } else if (file.endsWith('.java')) { // compile java
+
+      compile.java(file).then(_out => {
+          spinner.stop();
+          console.log(_out ? _out : 'Compilado')
+        },
+        err => {
+          spinner.stop();
+          console.log("Error:", err)
+        });
+    } else {
+      spinner.stop();
+      console.log("Error", args.file, " => No aceptado")
     }
   })
   /**
    * test command
    */
-  .command('test', 'Make the player walk')
+  .command('test', 'test source code java or cpp')
   .alias('t')
-  .argument('<file>', 'Codigo fuente .cpp, .java')
+  .argument('<file>', 'Codigo fuente *.cpp o *.java')
   .action((args, options, logger) => {
     console.log("Probando codigo: ", args)
   })
   /**
    * Describe un ejercicio
    */
-  .command('desc', 'Describe un ejercicio')
+  .command('desc', 'Describe exercise')
   .alias('d')
-  .argument('<number>', 'Numero del ejercicio')
+  .argument('<number>', 'Number of exercise')
   .action((args, options, logger) => {
-    console.log("Descripcion del ejercicio: ", args.number)
+    spinner.start("Loading exercise description " + args.number);    
+    // console.log("Descripcion del ejercicio: ", args.number)
     uhunt.byNum(args.number).then(data => {
-      console.log(data)
+      spinner.stop();
+      // console.log(data);
+      table.show(['pid', 'number', 'title', 'limit', 'status'], [data.pid, data.num, data.title, data.rtl, data.status])
     })
-    // logger.log("I'm walking !")
   })
   /**
    * Casos de pruebas
    */
   .command('cases', 'Casos de prueba')
-  .alias('c')
-  .argument('<number>', 'Numero del ejercicio')
+  .alias('cs')
+  .argument('<number>', 'Number of exercise')
   .action((args, options, logger) => {
-    console.log("Casos de prueba del ejercicio: ", args.number)
+    spinner.start("Loading test cases of the exercise " + args.number);
     udebug.get(args.number).then(data => {
-      console.log(data)
+      spinner.stop();
+
+      var test_cases = [];
+      data.map((item, index) => {
+        // test_cases.push(item.output.input_value);
+        console.log("-----------Start case-----------")
+        table.show(['#', 'Number', 'DataID', 'User', 'Date', 'Votes'], [(index + 1), item.number, item.dataid, item.user, item.date, item.votes])
+        console.log("Case ", (index + 1), " by ", item.user, ":");
+        console.log(item.output.input_value)
+        console.log("------------End case------------")
+      })
+      // console.log(test_cases)
+    });
+  })
+  /**
+   * Consultar los envios de un usuario
+   */
+  .command('submissions', 'Submissions by user')
+  .alias('sb')
+  .argument('<number>', 'Numero de usuario: ID User')
+  .action((args, options, logger) => {
+    spinner.start("Loading submissions by " + args.number);
+    uhunt.getUserSubmission(args.number).then(submiss => {
+      spinner.stop();
+
+      // console.log(submiss);
+      table.show(['name', 'user name'], [
+        submiss.name,
+        submiss.uname
+      ]);
+
+      var subs = [];
+      submiss.subs.map((item) => subs.push([item.reference, item.problem, item.result, item.time, item.code, item.date]));
+      table.multi(['Reference', 'problem', 'result', 'time', 'code', 'date'], subs, [11, 9, 21, 8, 8, 41]);
+    }, err => {
+      console.log("Error: ", submiss);
     })
     // logger.log("I'm walking !")
   })
   /**
    * Consultar los envios de un usuario
-   */  
-  .command('submissions', 'Casos de prueba')
-  .alias('s')
+   */
+  .command('solutions', 'Casos de prueba')
+  .alias('ss')
   .argument('<number>', 'Numero de usuario: ID User')
   .action((args, options, logger) => {
-    console.log("Consultando los envios de: ", args.number)
-    uhunt.getUserSubmission(args.number).then(submiss => {
-      console.log(submiss);
-    }, err => {
-      console.log("Error: ", submiss);
-    })
     // logger.log("I'm walking !")
   })
 
