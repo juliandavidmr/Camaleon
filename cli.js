183e58c725deee86cab747b2dc02a721ee54b8f4
juliandavidmr <anlijudavid@hotmail.com>
2017-03-11 21:38:14 -0500

    improvements

--- cli.js
+++ cli.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+var udebug = require("./lib/udebug.lib");
+var uhunt = require("uhunt-node");
 const prog = require('caporal');
 
 prog
@@ -18,6 +20,26 @@ prog
   .argument('<file>', 'Codigo fuente .cpp, .java')
   .action((args, options, logger) => {
     console.log("Probando codigo: ", args)
+    // logger.log("I'm walking !")
+  })
+  .command('desc', 'Describe un ejercicio')
+  .alias('d')
+  .argument('<number>', 'Numero del ejercicio')
+  .action((args, options, logger) => {
+    console.log("Descripcion del ejercicio: ", args.number)
+    uhunt.byNum(args.number).then(data => {
+      console.log(data)
+    })
+    // logger.log("I'm walking !")
+  })
+  .command('cases', 'Casos de prueba')
+  .alias('c')
+  .argument('<number>', 'Numero del ejercicio')
+  .action((args, options, logger) => {
+    console.log("Casos de prueba del ejercicio: ", args.number)
+    udebug.get(args.number).then(data => {
+      console.log(data)
+    })
     // logger.log("I'm walking !")
   })
 
